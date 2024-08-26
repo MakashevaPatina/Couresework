560c35a3587b515063752b684ca4835a683a774a
Patina Makasheva <patinamakasheva@gmail.com>
2024-08-26 20:18:19 +0300

    initial commit

---
+++ src/test/java/Javacore/Coursework/CourseworkApplicationTests.java
@@ -0,0 +1,13 @@
+package Javacore.Coursework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
